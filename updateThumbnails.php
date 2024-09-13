ff67ee5f2b7ae362e5c3c45151bcb09132c9ad6a
CCDC06 <ccdc06@proton.me>
2024-09-13 01:49:41 -0300

    wip

---
+++ updateThumbnails.php
@@ -0,0 +1,34 @@
+<?php
+namespace Metadata;
+require __DIR__ . '/functions.php';
+init();
+ini_set("memory_limit", -1);
+
+$zip = new \ZipArchive();
+$baseDir = require __DIR__ . '/temp/baseDir.php';
+
+$lfd = __DIR__ . '/temp/lastFile';
+if (!is_dir($lfd)) {
+	mkdir($lfd, 0777, true);
+}
+
+foreach (streamSpecs() as $spec) {
+	$fn = $baseDir . DIRECTORY_SEPARATOR . $spec->getBaseNameCbz();
+	if (!file_exists($fn)) {
+		continue;
+	}
+	echo $fn, PHP_EOL;
+
+	$zip->open($fn);
+	$sf = end($spec->Files);
+	if ($zip->statName($sf) === false) {
+		throw new Exception("BADFILES");
+	}
+	$lastFileName = $lfd . DIRECTORY_SEPARATOR . md5($spec->getBaseName()) . '_' . $sf;
+	if (file_exists($lastFileName)) {
+		continue;
+	}
+	file_put_contents($lastFileName, $zip->getStreamName($sf));
+
+	$zip->close();
+}
